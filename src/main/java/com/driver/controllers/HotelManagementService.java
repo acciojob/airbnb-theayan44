192493a76016d517b4d0c907fa6315aa83fb3af6
ayan <theayan44@gmail.com>
2023-07-04 12:13:36 +0530

    main

--- src/main/java/com/driver/controllers/HotelManagementService.java
+++ src/main/java/com/driver/controllers/HotelManagementService.java
@@ -12,8 +12,7 @@ import java.util.UUID;
 
 @Service
 public class HotelManagementService {
-    @Autowired
-    HotelManagementRepository hotelManagementRepository;
+    HotelManagementRepository hotelManagementRepository = new HotelManagementRepository();
     public String addHotel(Hotel hotel) {
         return hotelManagementRepository.addHotel(hotel);
     }
